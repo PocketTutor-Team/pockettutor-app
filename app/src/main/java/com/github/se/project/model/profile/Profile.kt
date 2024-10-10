75ea610707f7f3c7f9716172f9822c8da83f8b22
AlexandreM-0 <chaxav0@gmail.com>
2024-10-10 14:12:07 +0200

    Create interactive elements of tutor info screen. Import and update Lea's profile architecture, and save data to it.

---
+++ app/src/main/java/com/github/se/project/model/profile/Profile.kt
@@ -0,0 +1,72 @@
+package com.github.se.project.model.profile
+
+import java.util.EnumSet
+
+/** Data class representing a user profile. */
+data class Profile(
+    val uid: String, // Unique string id for the profile
+    val firstName: String, // First name of the user
+    val lastName: String, // Last name of the user
+    val role: Role, // Role of the user
+    val section: Section, // Section of the user
+    val academicLevel: AcademicLevel, // Academic level of the user
+    val email: String, // Email of the user => TODO: link to google sign-in
+    val languages : EnumSet<Language> = EnumSet.of(Language.STUDENT),
+    val subjects : EnumSet<TutoringSubject> = EnumSet.noneOf(TutoringSubject::class.java),
+    var price : Int = 0,
+    // TODO: profile picture
+)
+
+/** Enum classes representing the role, section, and academic level of a user. */
+enum class Role {
+    STUDENT,
+    TUTOR,
+    UNKNOWN
+    // Note: Use "UNKNOWN" for base value before the user selects a role
+    // but we should not create a profile without a role STUDENT or TUTOR
+}
+
+enum class Section {
+    IN,
+    SC, // School of Computer and Communication Sciences (IC)
+    AR,
+    GC,
+    SIE, // School of Architecture, Civil and Environmental Engineering (ENAC)
+    SGC,
+    MA,
+    PH, // School of Basic Sciences (SB)
+    EL,
+    MX,
+    GM,
+    MT, // School of Engineering (STI)
+    SV, // School of Life Sciences (SV)
+    NX, // Neuro-X Master (IC, STI, SV)
+    SIQ, // Quantum Science and Engineering (IC, SB, STI)
+}
+
+enum class AcademicLevel {
+    BA1,
+    BA2,
+    BA3,
+    BA4,
+    BA5,
+    BA6,
+    MA1,
+    MA2,
+    MA3,
+    MA4,
+    PhD,
+}
+
+enum class Language {
+    STUDENT,
+    FRENCH,
+    ENGLISH,
+    GERMAN,
+}
+
+enum class TutoringSubject {
+    ANALYSIS,
+    ALGEBRA,
+    PHYSICS,
+}
--- app/src/main/java/com/github/se/project/ui/screen/TutorInfoScreen.kt
+++ app/src/main/java/com/github/se/project/ui/screen/TutorInfoScreen.kt
@@ -5,10 +5,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,14 +20,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.github.se.project.model.profile.Language
+import com.github.se.project.model.profile.Profile
+import com.github.se.project.model.profile.TutoringSubject
 
 //Composable funtion to display the tutor sign up info screen
-@Preview(showBackground = true)
 @Composable
-fun TutorInfo(){
+fun TutorInfo(profile:Profile){
     val analyseChecked = remember { mutableStateOf(false) }
     val algebreChecked = remember { mutableStateOf(false) }
     val physiqueChecked = remember { mutableStateOf(false) }
@@ -33,19 +37,24 @@ fun TutorInfo(){
     val germanChecked = remember { mutableStateOf(false) }
     val average = 30
 
-    var expanded = remember { mutableStateOf(false) }.value
+    val expanded = remember { mutableStateOf(false) }
     val sliderValue = remember { mutableFloatStateOf(5f) }
+
+    val profilee = remember {mutableStateOf(profile)}
 
     Column { //Column composable to display the tutor sign up info
         Box(
             modifier = Modifier.fillMaxWidth(),
             contentAlignment = Alignment.Center
         ) {
-            Text("Welcome Tutor!", fontSize= 24.sp)
+            Text("Welcome Tutor!", fontSize = 24.sp)
         }
-        Text("Complete your profile by electing your capabilities:", Modifier.padding(start=4.dp))
+        Text("Complete your profile by electing your capabilities:", Modifier.padding(start = 4.dp))
 
-        Text("What languages do you feel comfortable teaching in?", Modifier.padding(start= 16.dp, top = 32.dp))
+        Text(
+            "What languages do you feel comfortable teaching in?",
+            Modifier.padding(start = 16.dp, top = 32.dp)
+        )
         Row(modifier = Modifier.padding(2.dp)) {
             Checkbox(
                 checked = frenchChecked.value,
@@ -64,26 +73,47 @@ fun TutorInfo(){
             Text("German", modifier = Modifier.padding(top = 14.dp))
         }
 
-        Text("Which subjects do you teach?", Modifier.padding(start= 16.dp, 4.dp))
+        Text("Which subjects do you teach?", Modifier.padding(start = 16.dp, 4.dp))
 
         Box {
-            Button(onClick = { expanded = true }) {
+            Button(onClick = { expanded.value = true }) {
                 Text("Select Subjects")
             }
             DropdownMenu(
-                expanded = expanded,
-                onDismissRequest = { expanded = false }
+                expanded = expanded.value,
+                onDismissRequest = { expanded.value = false }
             ) {
                 DropdownMenuItem(
-                    text = { Text("Analyse") },
+                    text = {
+                        Row {
+                            if (analyseChecked.value) {
+                                Icon(Icons.Filled.Check, contentDescription = null)
+                            }
+                            Text("Analyse")
+                        }
+                    },
                     onClick = { analyseChecked.value = !analyseChecked.value }
                 )
                 DropdownMenuItem(
-                    text = { Text("Algèbre") },
+                    text = {
+                        Row {
+                            if (algebreChecked.value) {
+                                Icon(Icons.Filled.Check, contentDescription = null)
+                            }
+                            Text("Algèbre")
+                        }
+                    },
                     onClick = { algebreChecked.value = !algebreChecked.value }
                 )
                 DropdownMenuItem(
-                    text = { Text("Physique") },
+                    text = {
+                        Row {
+                            if (physiqueChecked.value) {
+                                Icon(Icons.Filled.Check, contentDescription = null)
+                            }
+                            Text("Physique")
+                        }
+                    },
                     onClick = { physiqueChecked.value = !physiqueChecked.value }
                 )
             }
@@ -108,7 +138,10 @@ fun TutorInfo(){
         }*/
 
         // Slider price selection
-        Text("Select your tutoring price for an hour:", Modifier.padding(start = 16.dp, top = 16.dp))
+        Text(
+            "Select your tutoring price for an hour:",
+            Modifier.padding(start = 16.dp, top = 16.dp)
+        )
         Slider(
             value = sliderValue.floatValue,
             onValueChange = { sliderValue.floatValue = it },
@@ -118,16 +151,46 @@ fun TutorInfo(){
         )
         val priceDifference = average - sliderValue.floatValue.toInt()
         if (priceDifference >= 0)
-            Text("Your price is currently ${sliderValue.floatValue.toInt()}.-. This is $priceDifference.- less than the average price on PocketTutor.", Modifier.padding(start = 16.dp, top = 8.dp))
+            Text(
+                "Your price is currently ${sliderValue.floatValue.toInt()}.-. This is $priceDifference.- less than the average price on PocketTutor.",
+                Modifier.padding(start = 16.dp, top = 8.dp)
+            )
         else
-            Text("Your price is currently ${sliderValue.floatValue.toInt()}.-. This is ${-priceDifference}.- more than the average price on PocketTutor.", Modifier.padding(start = 16.dp, top = 8.dp))
+            Text(
+                "Your price is currently ${sliderValue.floatValue.toInt()}.-. This is ${-priceDifference}.- more than the average price on PocketTutor.",
+                Modifier.padding(start = 16.dp, top = 8.dp)
+            )
 
         //Button confirming the tutor sign up info
         Button(
-            onClick = { /*TODO: Add navigation and save info to firebase*/ },
+            onClick = {
+                profile.languages.remove(Language.STUDENT)
+                if (frenchChecked.value) {
+                    profilee.value.languages.add(Language.FRENCH)
+                }
+                if (englishChecked.value) {
+                    profilee.value.languages.add(Language.ENGLISH)
+                }
+                if (germanChecked.value) {
+                    profile.languages.add(Language.GERMAN)
+                }
+                if (analyseChecked.value) {
+                    profilee.value.subjects.add(TutoringSubject.ANALYSIS)
+                }
+                if (algebreChecked.value) {
+                    profile.subjects.add(TutoringSubject.ALGEBRA)
+                }
+                if (physiqueChecked.value) {
+                    profile.subjects.add(TutoringSubject.PHYSICS)
+                }
+                profilee.value.price = sliderValue.floatValue.toInt()
+                /*TODO: Add navigation and save info to firebase*/
+            },
             modifier = Modifier.align(Alignment.CenterHorizontally).padding(16.dp)
         ) {
             Text("Confirm")
         }
+        Text(frenchChecked.toString())
+        Text(profilee.toString())
     }
 }
