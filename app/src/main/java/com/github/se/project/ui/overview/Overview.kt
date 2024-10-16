b73b793211dec55042ff638f64f777473b3f4b1f
MPepito <maik.pasquier@epfl.ch>
2024-10-16 10:29:22 +0200

    feat: add an Icon in the top bar that allow the user to nagivate to the profilInfoScreen

--- app/src/main/java/com/github/se/project/ui/overview/Overview.kt
+++ app/src/main/java/com/github/se/project/ui/overview/Overview.kt
@@ -11,8 +11,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -76,6 +80,21 @@ fun HomeScreen(
       }
 
   Scaffold(
+      topBar = {
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(16.dp),
+            horizontalArrangement = Arrangement.End,
+            verticalAlignment = Alignment.CenterVertically) {
+              IconButton(
+                  onClick = {
+                    // Do nothing for now or add any logic you want when the button is clicked
+                    Toast.makeText(context, "Profile icon clicked", Toast.LENGTH_SHORT).show()
+                    navigationActions.navigateTo(Screen.PROFILE)
+                  }) {
+                    Icon(imageVector = Icons.Filled.AccountBox, contentDescription = "Profile Icon")
+                  }
+            }
+      },
       bottomBar = {
         BottomNavigationMenu(
             onTabSelect = { route -> navigationActions.navigateTo(route) },
