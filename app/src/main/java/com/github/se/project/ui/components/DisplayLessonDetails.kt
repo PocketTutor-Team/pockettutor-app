bc606ba9b6488bd9ba478906b6e5cfc9ec71b428
blancher <lea.blancher@epfl.ch>
2024-10-31 14:34:49 +0100

    feat: Add displayed composable for cleaner UI
    Update the composable to be coherent with the global theme and UI.
    Add test tag to enable testing.

--- app/src/main/java/com/github/se/project/ui/components/DisplayLessonDetails.kt
+++ app/src/main/java/com/github/se/project/ui/components/DisplayLessonDetails.kt
@@ -1,11 +1,18 @@
 package com.github.se.project.ui.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.unit.dp
 import com.github.se.project.model.lesson.Lesson
 import com.github.se.project.model.profile.Profile
 
@@ -14,30 +21,53 @@ fun DisplayLessonDetails(
     lesson: Lesson,
     studentProfile: Profile,
 ) {
-  Column(
-      modifier = Modifier.fillMaxSize(),
-      content = {
-        // Student information
-        Row(
+  Card(
+      modifier =
+          Modifier.fillMaxWidth()
+              .padding(vertical = 2.dp)
+              .testTag("lessonDetailsCard"), // Tag for each lesson card
+      colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceBright)) {
+        Column(
             content = {
-              // TODO: Add profile image
-
-              // Name and Academic Level
-              Column(
+              // Student information
+              Row(
+                  modifier = Modifier.padding(16.dp).fillMaxWidth(),
+                  horizontalArrangement = Arrangement.SpaceBetween,
                   content = {
-                    Text(studentProfile.firstName + studentProfile.lastName)
-                    Text(
-                        studentProfile.section.toString() +
-                            " " +
-                            studentProfile.academicLevel.toString())
+                    // TODO: Add profile image
+
+                    // Name and Academic Level
+                    Column(
+                        content = {
+                          Text(
+                              studentProfile.firstName + studentProfile.lastName,
+                              style = MaterialTheme.typography.titleMedium,
+                              modifier = Modifier.testTag("studentName"))
+                          Text(
+                              studentProfile.section.toString() +
+                                  " " +
+                                  studentProfile.academicLevel.toString(),
+                              style = MaterialTheme.typography.bodyMedium,
+                          )
+                        })
+
+                    // Price range
+                    Text(lesson.minPrice.toString() + ".-/" + lesson.maxPrice.toString() + ".-")
                   })
 
-              // Price range
-              Text(lesson.minPrice.toString() + ".-/" + lesson.maxPrice.toString() + ".-")
+              // Lesson details
+              Column(
+                  modifier = Modifier.padding(16.dp),
+              ) {
+                Text(
+                    lesson.title,
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.testTag("lessonTitle"))
+                Text(
+                    lesson.description,
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.testTag("lessonDescription"))
+              }
             })
-
-        // Lesson details
-        Text(lesson.title)
-        Text(lesson.description)
-      })
+      }
 }
--- app/src/main/java/com/github/se/project/ui/lesson/TutorLessonResponse.kt
+++ app/src/main/java/com/github/se/project/ui/lesson/TutorLessonResponse.kt
@@ -2,21 +2,24 @@ package com.github.se.project.ui.lesson
 
 import android.widget.Toast
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Scaffold
-import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
@@ -41,31 +44,35 @@ fun TutorLessonResponseScreen(
       lessonViewModel.selectedLesson.collectAsState().value
           ?: return Text("No lesson selected. Should not happen.")
 
-  val sliderPrice = remember { mutableFloatStateOf(lesson.price.toFloat()) }
+  val sliderPrice = remember {
+    mutableFloatStateOf(((lesson.maxPrice - lesson.minPrice).toFloat() / 2.0).toFloat())
+  }
 
   val context = LocalContext.current
 
   Scaffold(
       topBar = {
-        TopAppBar(
-            title = {
+        Row(
+            modifier = Modifier.testTag("topBar").fillMaxWidth().padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically) {
+              IconButton(onClick = { navigationActions.goBack() }) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = "Back arrow",
+                    modifier = Modifier.size(32.dp).testTag("backButton"))
+              }
+
               Text(
-                  "Respond to the request", modifier = Modifier.testTag("TutorLessonResponseTitle"))
-            },
-            navigationIcon = {
-              IconButton(
-                  onClick = { navigationActions.goBack() },
-                  modifier = Modifier.testTag("TutorLessonResponseBackButton")) {
-                    Icon(
-                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                        contentDescription = "Back")
-                  }
-            })
+                  text = "Respond to the request",
+                  style = MaterialTheme.typography.headlineMedium,
+                  modifier = Modifier.testTag("TutorLessonResponseTitle"))
+            }
       },
       content = { paddingValues ->
         Column(
             modifier =
                 Modifier.fillMaxSize()
+                    .padding(16.dp)
                     .padding(paddingValues)
                     .testTag("tutorLessonResponseScreen")) {
               val studentProfile = listProfilesViewModel.getProfileById(lesson.studentUid)
