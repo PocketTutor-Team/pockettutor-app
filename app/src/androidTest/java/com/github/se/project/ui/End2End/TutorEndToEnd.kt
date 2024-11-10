481051a719e61751b3818f893a408a611fc7bd04
Alessandro Ferrante <alessandro.ferrante@epfl.ch>
2024-11-10 16:06:35 +0100

    Commit before pulling main

--- app/src/androidTest/java/com/github/se/project/ui/End2End/TutorEndToEnd.kt
+++ app/src/androidTest/java/com/github/se/project/ui/End2End/TutorEndToEnd.kt
@@ -1,11 +1,13 @@
 package com.github.se.project.ui.End2End
 
 import android.content.Context
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performGesture
 import androidx.compose.ui.test.performTextInput
@@ -19,9 +21,9 @@ import com.github.se.project.model.lesson.LessonStatus
 import com.github.se.project.model.lesson.LessonViewModel
 import com.github.se.project.model.profile.Language
 import com.github.se.project.model.profile.ListProfilesViewModel
+import com.github.se.project.model.profile.Profile
 import com.github.se.project.model.profile.ProfilesRepository
-import com.github.se.project.model.profile.Role
-import com.github.se.project.model.profile.Subject
+import com.github.se.project.model.profile.*
 import com.github.se.project.ui.navigation.NavigationActions
 import kotlinx.coroutines.flow.MutableStateFlow
 import org.junit.Assert.assertEquals
@@ -30,7 +32,10 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
+import org.mockito.Mockito.doNothing
+import org.mockito.Mockito.doReturn
 import org.mockito.Mockito.mock
+import org.mockito.Mockito.spy
 import org.mockito.kotlin.any
 import org.mockito.kotlin.whenever
 
@@ -46,9 +51,9 @@ class EndToEndTest {
 
   private val mockProfileViewModel = ListProfilesViewModel(mockProfileRepository)
 
-  private val mockLessonRepository = mock(LessonRepository::class.java)
+  private lateinit var mockLessonRepository : LessonRepository
 
-  private val mockLessonViewModel = LessonViewModel(mockLessonRepository)
+  private lateinit var mockLessonViewModel : LessonViewModel
 
   private val mockLessons =
     listOf(
@@ -58,12 +63,12 @@ class EndToEndTest {
         description = "Mechanics and Thermodynamics",
         subject = Subject.PHYSICS,
         languages = listOf(Language.ENGLISH),
-        tutorUid = "mockUid",
+        tutorUid = listOf("mockUid"),
         studentUid = "student123",
         minPrice = 20.0,
         maxPrice = 40.0,
         timeSlot = "2024-10-10T10:00:00",
-        status = LessonStatus.PENDING,
+        status = LessonStatus.STUDENT_REQUESTED,
         latitude = 0.0,
         longitude = 0.0),
       Lesson(
@@ -72,27 +77,52 @@ class EndToEndTest {
         description = "Algebra and Calculus",
         subject = Subject.ANALYSIS,
         languages = listOf(Language.ENGLISH),
-        tutorUid = "mockUid",
+        tutorUid = listOf("mockUid"),
         studentUid = "student123",
         minPrice = 20.0,
         maxPrice = 40.0,
         timeSlot = "2024-10-10T11:00:00",
-        status = LessonStatus.PENDING,
+        status = LessonStatus.STUDENT_REQUESTED,
         latitude = 0.0,
         longitude = 0.0)
     )
+
+  private val mockStudent = Profile(
+    "student123",
+    "mockTutor",
+    "Ozymandias",
+    "Halifax",
+    "1234567890",
+    Role.STUDENT,
+    Section.IN,
+    AcademicLevel.BA3,
+    listOf(Language.ENGLISH),
+    listOf(Subject.AICC),
+    List(7) { List(12) { 0 } })
+
   private val requestedLessonsFlow = MutableStateFlow(mockLessons)
 
   @get:Rule val composeTestRule = createComposeRule()
 
   @Before
   fun setUp() {
-    context = mock(Context::class.java)
     whenever(mockProfileRepository.addProfile(any(), any(), any())).thenAnswer { invocation ->
       val onSuccess = invocation.arguments[1] as () -> Unit
       onSuccess() // Simulate a successful update
     }
+    whenever(mockProfileRepository.getProfiles(any(), any())).thenAnswer { invocation ->
+      val onSuccess = invocation.arguments[0] as (List<Profile>) -> Unit
+      onSuccess(listOf(mockStudent)) // Simulate a list of profiles with our beloved Ozymandias
+    }
     whenever(mockProfileRepository.getNewUid()).thenReturn("mockUid")
+
+    mockLessonRepository = mock(LessonRepository::class.java)
+    mockLessonViewModel = LessonViewModel(mockLessonRepository)
+    mockLessonViewModel = spy(mockLessonViewModel)
+
+    doReturn(requestedLessonsFlow).`when`(mockLessonViewModel).requestedLessons
+    doNothing().`when`(mockLessonRepository).getAllRequestedLessons(any(), any())
+
   }
 
   @Test
@@ -147,8 +177,10 @@ class EndToEndTest {
         .assertTextEquals("0 lessons given since you joined PocketTutor")
     composeTestRule.onNodeWithTag("closeButton").performClick()
     composeTestRule.onNodeWithTag("Find a Student").performClick()
-    composeTestRule.onNodeWithTag("Find a Student").performClick()
-    composeTestRule.onNodeWithTag("My Work Space").performClick()
+      composeTestRule.onNodeWithTag("screenTitle").assertExists()
+    val filteredDate = "2024-10-10"
+    requestedLessonsFlow.value = mockLessons.filter { it.timeSlot.contains(filteredDate) }
+
 
     Thread.sleep(5000)
   }
