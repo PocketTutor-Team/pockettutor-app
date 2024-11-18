76a168d940b53e4878f76b3d2e458d1e904a5a4b
Alexis <alexis.rufer@epfl.ch>
2024-11-18 13:30:13 +0100

    style: applied KTFMT format

--- app/src/main/java/com/github/se/project/model/profile/Rating.kt
+++ app/src/main/java/com/github/se/project/model/profile/Rating.kt
@@ -5,13 +5,14 @@ data class Rating(
     var totalRatings: Int = 0, // total number of ratings
     val comments: MutableList<Comment> = mutableListOf() // list of comments
 ) {
-    /**
-     * Adds a new rating and updates the average.
-     * @param comment the Comment object representing the new rating.
-     */
-    fun addRating(comment: Comment) {
-        comments.add(comment)
-        totalRatings += 1
-        averageRating = comments.sumOf { it.grade.toDouble() } / totalRatings
-    }
+  /**
+   * Adds a new rating and updates the average.
+   *
+   * @param comment the Comment object representing the new rating.
+   */
+  fun addRating(comment: Comment) {
+    comments.add(comment)
+    totalRatings += 1
+    averageRating = comments.sumOf { it.grade.toDouble() } / totalRatings
+  }
 }
