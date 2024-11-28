05727a2d53679213351f77b8e1476435b4a1e5bc
aferrante1 <160861502+aferrante1@users.noreply.github.com>
2024-11-28 14:22:43 +0100

    Update OverViewTest.kt
    The token variable in Profile and Lesson is not yet implemented

--- app/src/androidTest/java/com/github/se/project/ui/overview/OverViewTest.kt
+++ app/src/androidTest/java/com/github/se/project/ui/overview/OverViewTest.kt
@@ -52,7 +52,7 @@ class HomeScreenTest {
   private val profile =
       Profile(
           "uid",
-          "",
+          //"",
           "googleUid",
           "firstName",
           "lastName",
@@ -76,7 +76,7 @@ class HomeScreenTest {
               languages = listOf(Language.ENGLISH),
               tutorUid = listOf("tutor123"),
               studentUid = "student123",
-              StudentToken = "",
+              //StudentToken = "",
               minPrice = 20.0,
               maxPrice = 40.0,
               timeSlot = "2024-10-10T10:00:00",
