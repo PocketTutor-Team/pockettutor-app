6b74b760f92fb45a44f2f6834878ad56706a0580
Alexis <alexis.rufer@epfl.ch>
2024-12-02 12:40:37 +0100

    test: timplemented tests for PhoneNumberInput.kt component file

---
+++ app/src/androidTest/java/com/github/se/project/ui/components/PhoneNumberInputTest.kt
@@ -0,0 +1,76 @@
+package com.github.se.project.ui.components
+
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createComposeRule
+import com.github.se.project.utils.countries
+import org.junit.Rule
+import org.junit.Test
+
+class PhoneNumberInputTest {
+
+  @get:Rule val composeTestRule = createComposeRule()
+
+  @Test
+  fun testPhoneNumberInputRenders() {
+    val selectedCountry = countries[0]
+    val phoneNumber = ""
+    composeTestRule.setContent {
+      PhoneNumberInput(
+          selectedCountry = selectedCountry,
+          onCountryChange = {},
+          phoneNumber = phoneNumber,
+          onPhoneNumberChange = {})
+    }
+
+    // Check that the Country Code field is displayed
+    composeTestRule.onNodeWithTag("countryCodeField").assertIsDisplayed()
+
+    // Check that the Phone Number field is displayed
+    composeTestRule.onNodeWithTag("phoneNumberField").assertIsDisplayed()
+  }
+
+  @Test
+  fun testPhoneNumberInputUpdatesValue() {
+    var currentPhoneNumber = ""
+    composeTestRule.setContent {
+      PhoneNumberInput(
+          selectedCountry = countries[0],
+          onCountryChange = {},
+          phoneNumber = currentPhoneNumber,
+          onPhoneNumberChange = { currentPhoneNumber = it })
+    }
+
+    // Enter a phone number
+    composeTestRule.onNodeWithTag("phoneNumberField").performTextInput("1234567890")
+
+    // Check that the phone number is updated
+    assert(currentPhoneNumber == "1234567890")
+  }
+
+  @Test
+  fun testCountryCodeDropdown() {
+    var currentSelectedCountry = countries[0]
+    composeTestRule.setContent {
+      PhoneNumberInput(
+          selectedCountry = currentSelectedCountry,
+          onCountryChange = { currentSelectedCountry = it },
+          phoneNumber = "",
+          onPhoneNumberChange = {})
+    }
+
+    // Open the country code dropdown
+    composeTestRule.onNodeWithTag("countryCodeField").performClick()
+
+    // Wait for the dropdown to appear
+    composeTestRule.waitForIdle()
+
+    // Select the second country in the list
+    val secondCountry = countries[1]
+    val testTag = "country_${secondCountry.code.replace("+", "plus")}"
+
+    composeTestRule.onNodeWithTag(testTag).performClick()
+
+    // Check that the selected country has been updated
+    assert(currentSelectedCountry == secondCountry)
+  }
+}
