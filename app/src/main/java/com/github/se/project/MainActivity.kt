8ac61bbeec5254230e290d9debbda4868053bec0
Alexis <alexis.rufer@epfl.ch>
2024-12-02 22:03:50 +0100

    feat: implemented the auto log in feature in the Mainactivty.kt

--- app/src/main/java/com/github/se/project/MainActivity.kt
+++ app/src/main/java/com/github/se/project/MainActivity.kt
@@ -4,9 +4,15 @@ import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -39,6 +45,7 @@ import com.github.se.project.ui.profile.EditProfile
 import com.github.se.project.ui.profile.EditTutorSchedule
 import com.github.se.project.ui.profile.ProfileInfoScreen
 import com.github.se.project.ui.theme.SampleAppTheme
+import com.google.firebase.auth.FirebaseAuth
 import io.getstream.chat.android.client.ChatClient
 import io.getstream.chat.android.client.logger.ChatLogLevel
 import io.getstream.chat.android.offline.plugin.factory.StreamOfflinePluginFactory
@@ -103,45 +110,51 @@ fun PocketTutorApp(
   val navController = rememberNavController()
   val navigationActions = NavigationActions(navController)
 
-  val profiles = listProfilesViewModel.profiles
+  // retrieve the current Firebase user only if logged in
+  val user = FirebaseAuth.getInstance().currentUser
+
+  val profiles by listProfilesViewModel.profiles.collectAsState(initial = emptyList())
 
   // Google user unique id (as var to be able to pass from the SignIn to CreateProfile screens)
   var googleUid = ""
 
-  // Context
   val context = LocalContext.current
 
-  NavHost(navController = navController, startDestination = Route.AUTH) {
-    // Authentication flow
-    navigation(startDestination = Screen.AUTH, route = Route.AUTH) {
-      composable(Screen.AUTH) {
-        if (testMode) {
-          SignInScreen(
-              onSignInClick = {
-                googleUid = "testingUid"
-                navigationActions.navigateTo(Screen.CREATE_PROFILE)
-              })
+  // side-effect to handle navigation and profile setup, based on user and profiles state
+  LaunchedEffect(user, profiles) {
+    if (user != null) { // checks if the user is logged in
+      if (profiles.isNotEmpty()) { // ensures profiles are loaded
+        val profile = profiles.find { it.googleUid == user.uid }
+        if (profile != null) {
+          // if a profile exists for the user, set it as the current profile
+          listProfilesViewModel.setCurrentProfile(profile)
+          // navigate to the Home screen and clear the back stack
+          navController.navigate(Screen.HOME) { popUpTo(Screen.AUTH) { inclusive = true } }
         } else {
-          SignInScreen(
-              onSignInClick = {
-                authenticationViewModel.handleGoogleSignIn(
-                    context,
-                    onSuccess = { uid ->
-                      googleUid = uid
-                      val profile = profiles.value.find { it.googleUid == googleUid }
-
-                      if (profile != null) {
-                        // If the user already has a profile, navigate to the home screen
-                        listProfilesViewModel.setCurrentProfile(profile)
-                        navigationActions.navigateTo(Screen.HOME)
-                      } else {
-                        // If the user doesn't have a profile, navigate to the profile creation
-                        // screen
-                        navigationActions.navigateTo(Screen.CREATE_PROFILE)
-                      }
-                    })
-              })
+          // if no profile exists, navigate to the profile creation screen
+          navController.navigate(Screen.CREATE_PROFILE) {
+            popUpTo(Screen.AUTH) { inclusive = true }
+          }
         }
+      }
+      // If profiles are still loading, wait for recomposition
+    } else {
+      // If no user is logged in, navigate to the authentication screen
+      navController.navigate(Screen.AUTH) { popUpTo(Screen.AUTH) { inclusive = true } }
+    }
+  }
+
+  // put it in a box to be able to display the loading indicator
+  Box(modifier = Modifier.fillMaxSize()) {
+    // Navigation host for the app
+    NavHost(navController = navController, startDestination = Screen.AUTH) {
+      composable(Screen.AUTH) {
+        SignInScreen(
+            onSignInClick = {
+              // handle Google sign-in and navigate to the Home screen on success
+              authenticationViewModel.handleGoogleSignIn(
+                  context = context, onSuccess = { navController.navigate(Screen.HOME) })
+            })
       }
 
       composable(Screen.HOME) {
@@ -170,68 +183,75 @@ fun PocketTutorApp(
         ChannelScreen(navigationActions, listProfilesViewModel, chatViewModel, lessonViewModel)
       }
       composable(Screen.CHAT) { ChatScreen(navigationActions, chatViewModel) }
+
+      navigation(
+          startDestination = Screen.LESSONS_REQUESTED,
+          route = Route.FIND_STUDENT,
+      ) {
+        composable(Screen.HOME) {
+          HomeScreen(listProfilesViewModel, lessonViewModel, chatViewModel, navigationActions)
+        }
+        composable(Screen.LESSONS_REQUESTED) {
+          RequestedLessonsScreen(listProfilesViewModel, lessonViewModel, navigationActions)
+        }
+        composable(Screen.TUTOR_LESSON_RESPONSE) {
+          TutorLessonResponseScreen(listProfilesViewModel, lessonViewModel, navigationActions)
+        }
+        composable(Screen.CONFIRMED_LESSON) {
+          ConfirmedLessonScreen(listProfilesViewModel, lessonViewModel, navigationActions)
+        }
+      }
+
+      navigation(
+          startDestination = Screen.HOME,
+          route = Route.HOME,
+      ) {
+        composable(Screen.HOME) {
+          HomeScreen(listProfilesViewModel, lessonViewModel, chatViewModel, navigationActions)
+        }
+        composable(Screen.EDIT_PROFILE) { EditProfile(navigationActions, listProfilesViewModel) }
+        composable(Screen.EDIT_SCHEDULE) {
+          EditTutorSchedule(navigationActions, listProfilesViewModel)
+        }
+        composable(Screen.EDIT_REQUESTED_LESSON) {
+          EditRequestedLessonScreen(
+              navigationActions,
+              listProfilesViewModel,
+              lessonViewModel,
+              onMapReadyChange = onMapReadyChange)
+        }
+        composable(Screen.TUTOR_LESSON_RESPONSE) {
+          TutorLessonResponseScreen(listProfilesViewModel, lessonViewModel, navigationActions)
+        }
+      }
+
+      navigation(
+          startDestination = Screen.ADD_LESSON,
+          route = Route.FIND_TUTOR,
+      ) {
+        composable(Screen.HOME) {
+          HomeScreen(listProfilesViewModel, lessonViewModel, chatViewModel, navigationActions)
+        }
+        composable(Screen.ADD_LESSON) {
+          AddLessonScreen(
+              navigationActions,
+              listProfilesViewModel,
+              lessonViewModel,
+              onMapReadyChange = onMapReadyChange)
+        }
+        composable(Screen.TUTOR_MATCH) {
+          TutorMatchingScreen(listProfilesViewModel, lessonViewModel, navigationActions)
+        }
+        composable(Screen.SELECTED_TUTOR_DETAILS) {
+          SelectedTutorDetailsScreen(listProfilesViewModel, lessonViewModel, navigationActions)
+        }
+      }
     }
 
-    navigation(
-        startDestination = Screen.LESSONS_REQUESTED,
-        route = Route.FIND_STUDENT,
-    ) {
-      composable(Screen.HOME) {
-        HomeScreen(listProfilesViewModel, lessonViewModel, chatViewModel, navigationActions)
-      }
-      composable(Screen.LESSONS_REQUESTED) {
-        RequestedLessonsScreen(listProfilesViewModel, lessonViewModel, navigationActions)
-      }
-      composable(Screen.TUTOR_LESSON_RESPONSE) {
-        TutorLessonResponseScreen(listProfilesViewModel, lessonViewModel, navigationActions)
-      }
-      composable(Screen.CONFIRMED_LESSON) {
-        ConfirmedLessonScreen(listProfilesViewModel, lessonViewModel, navigationActions)
-      }
-    }
-
-    navigation(
-        startDestination = Screen.HOME,
-        route = Route.HOME,
-    ) {
-      composable(Screen.HOME) {
-        HomeScreen(listProfilesViewModel, lessonViewModel, chatViewModel, navigationActions)
-      }
-      composable(Screen.EDIT_PROFILE) { EditProfile(navigationActions, listProfilesViewModel) }
-      composable(Screen.EDIT_SCHEDULE) {
-        EditTutorSchedule(navigationActions, listProfilesViewModel)
-      }
-      composable(Screen.EDIT_REQUESTED_LESSON) {
-        EditRequestedLessonScreen(
-            navigationActions,
-            listProfilesViewModel,
-            lessonViewModel,
-            onMapReadyChange = onMapReadyChange)
-      }
-      composable(Screen.TUTOR_LESSON_RESPONSE) {
-        TutorLessonResponseScreen(listProfilesViewModel, lessonViewModel, navigationActions)
-      }
-    }
-
-    navigation(
-        startDestination = Screen.ADD_LESSON,
-        route = Route.FIND_TUTOR,
-    ) {
-      composable(Screen.HOME) {
-        HomeScreen(listProfilesViewModel, lessonViewModel, chatViewModel, navigationActions)
-      }
-      composable(Screen.ADD_LESSON) {
-        AddLessonScreen(
-            navigationActions,
-            listProfilesViewModel,
-            lessonViewModel,
-            onMapReadyChange = onMapReadyChange)
-      }
-      composable(Screen.TUTOR_MATCH) {
-        TutorMatchingScreen(listProfilesViewModel, lessonViewModel, navigationActions)
-      }
-      composable(Screen.SELECTED_TUTOR_DETAILS) {
-        SelectedTutorDetailsScreen(listProfilesViewModel, lessonViewModel, navigationActions)
+    // display a loading indicator while profiles are loading
+    if (user != null && profiles.isEmpty()) {
+      Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        CircularProgressIndicator() // Show a spinning loader
       }
     }
   }
