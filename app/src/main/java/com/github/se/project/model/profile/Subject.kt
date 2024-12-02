b0aa507ba750e6c0ece802c0bb8ea39b4a947a15
Philipp0206 <philipp.glaser@epfl.ch>
2024-12-02 14:28:25 +0100

    chore: cleaned subject.kt

--- app/src/main/java/com/github/se/project/model/profile/Subject.kt
+++ app/src/main/java/com/github/se/project/model/profile/Subject.kt
@@ -2,20 +2,10 @@ package com.github.se.project.model.profile
 
 /** Enum classes representing the subject of a user. */
 enum class Subject {
-  ADVANCED_ALGEBRA,
-  ALGEBRA,
-  /*ALGORITHM,
-  ADVANCED_ANALYSIS,
   ANALYSIS,
-  BIOLOGY,
-  CHEMISTRY,
-  AICC,
-  ICC,
-  COMPUTER_ARCHITECTURE,
-  ADVANCED_PHYSICS,
+  ALGEBRA,
   PHYSICS,
-  SIGNAL_PROCESSING,
-  SOFTWARE_CONSTRUCTION,
-   */
+  ICC,
+  AICC,
   NONE
 }
